3afd931cf1248373f26fd814e983839181a63337
Vidit Jain <viditjain@live.com>
2022-01-21 15:39:03 +0530

    Updated C++ snippet

--- Template/C++ Snippet.cpp
+++ Template/C++ Snippet.cpp
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
 #define MOD 1000000007
 typedef long long ll;
 //#define int ll 
 
-typedef pair<ll, ll> ii;
-typedef vector<ll> vi;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
 typedef vector<bool> vb;
 typedef vector<vi> vvi;
 typedef vector<ii> vii;
@@ -16,11 +24,24 @@ typedef vector<vii> vvii;
 #define pb push_back
 #define all(s) s.begin(), s.end()
 #define tc int t; cin>>t; while(t--)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
 #define file_read(x,y) freopen(x, "r", stdin); \
 						freopen(y, "w", stdout);
 #define fightFight cin.tie(0) -> sync_with_stdio(0)
+#define show_vi(a)        \
+	cout << #a << "-->";  \
+	for (auto &x : a) cout << x << " "; \
+	cout << endl;
+
+void prec() {};
+
+void solve(){
+	
+}
 
 int main(){
 	fightFight;
-	
+	prec();
+	solve();   
 }
